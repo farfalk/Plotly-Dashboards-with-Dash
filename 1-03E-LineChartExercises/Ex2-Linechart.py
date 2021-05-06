dbabb3e759017fab9bd71301c472c040e710c09d
farfalk <talevi.luca@gmail.com>
2021-05-06 20:07:15 +0200

    Exercise 2.

--- 1-03E-LineChartExercises/Ex2-Linechart.py
+++ 1-03E-LineChartExercises/Ex2-Linechart.py
@@ -6,26 +6,27 @@
 
 # Perform imports here:
 
-
+import plotly.offline as pyo
+import plotly.graph_objects as go
+import pandas as pd
 
 
 # Create a pandas DataFrame from 2010YumaAZ.csv
-df = pd.read_csv('../data/2010YumaAZ.csv')
-days = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
-
+df: pd.DataFrame = pd.read_csv('data/2010YumaAZ.csv')
+days: list = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
 
 # Use a for loop (or list comprehension to create traces for the data list)
-data = []
-
-for day in days:
-    # What should go inside this Scatter call?
-    trace = go.Scatter()
-    data.append(trace)
+data: list = [  go.Scatter(  x=df.loc[df['DAY']==day]['LST_TIME'],
+                            y=df.loc[df['DAY']==day]['T_HR_AVG'],
+                            mode='markers+lines',
+                            name=day)
+                for day in days]
 
 # Define the layout
 
-
-
-
+layout: go.Layout = go.Layout(title = 'Temperature data (2010 data, Yuma AZ)')
 
 # Create a fig from data and layout, and plot the fig
+
+fig: go.Figure = go.Figure(data=data,layout=layout)
+pyo.plot(fig, filename='1-03E-LineChartExercises/my_solution.html')
